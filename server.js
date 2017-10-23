abe6accca17ba8ece69e3f2360ed05c992c16b70
jamie <chming28@mail.apex.com.tw>
2017-10-23 17:31:08 +0800

    fix cors

--- server.js
+++ server.js
@@ -10,7 +10,14 @@ const app = express()
 require('./configs/database')
 
 app.use(bodyParser.urlencoded({ extended: false })).use(bodyParser.json())
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  next()
+})
 // register all routers
 // all routes are prefixed with /api
 app.use('/api', require('./routes/credit'))
